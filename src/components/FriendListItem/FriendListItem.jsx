7959d9bb72a1b7336d4d0ca994bc8545fcc984f5
Roman Vashchenko <roman1991dp@gmail.com>
2024-05-23 13:53:11 +0300

    update

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import css from "../FriendList/FriendListItem.module.css";
+import css from "../FriendListItem/FriendListItem.module.css";
 
 const FriendListItem = ({ name, avatar, isOnline }) => {
   return (
